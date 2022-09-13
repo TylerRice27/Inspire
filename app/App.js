e01905751c9a7acd76d03c3d0fa8f5372160eb94
TylerRice27 <Tylerrayrice2@gmail.com>
2022-09-13 14:39:39 -0600

    Gutting content

--- app/App.js
+++ app/App.js
@@ -1,10 +1,15 @@
+import { QuotesController } from "./Controllers/QuotesController.js";
 import { TodosController } from "./Controllers/TodosController.js";
 import { ValuesController } from "./Controllers/ValuesController.js";
+import { WeathersController } from "./Controllers/WeathersController.js";
 
 class App {
   // valuesController = new ValuesController();
 
   todosController = new TodosController()
+  quotesController = new QuotesController()
+  weathersController = new WeathersController()
+
 
 
 
