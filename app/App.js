77cb2d312421f9dce91320c71adaa9fadeb713dc
TylerRice27 <Tylerrayrice2@gmail.com>
2022-09-14 16:32:20 -0600

    working on clock feature right now

--- app/App.js
+++ app/App.js
@@ -4,15 +4,39 @@ import { ValuesController } from "./Controllers/ValuesController.js";
 import { WeathersController } from "./Controllers/WeathersController.js";
 
 
-function _drawClock() {
-  var clock = new Date("2012-01-18T16:03");
+// might need a .slice here
 
-  // might need a .slice here
-  console.log((clock.getMinutes() < 10 ? '0' : '') + clock.getMinutes());
+function currentTime() {
+  let date = new Date();
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
+  let session = "AM";
+
+  if (hh == 0) {
+    hh = 12;
+  }
+  if (hh > 12) {
+    hh = hh - 12;
+    session = "PM";
+  }
+
+  hh = (hh < 10) ? "0" + hh : hh;
+  mm = (mm < 10) ? "0" + mm : mm;
+  ss = (ss < 10) ? "0" + ss : ss;
+
+  let time = hh + ":" + mm + ":" + ss + " " + session;
+
+  document.getElementById("clock").innerText = time;
+  let t = setTimeout(function () { currentTime() }, 1000);
 }
+currentTime();
+
 
 class App {
   // valuesController = new ValuesController();
+
+
 
   todosController = new TodosController()
   quotesController = new QuotesController()
