d878c8876cf538d56f2e6f57d185a02e068e7efe
TylerRice27 <Tylerrayrice2@gmail.com>
2022-09-14 09:28:57 -0600

    started to todo model

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -4,7 +4,10 @@ export class Todo {
 
     constructor(data) {
 
-
+        this.id = data.id
+        this.completed = data.completed
+        this.user = data.user
+        this.description = data.description
 
     }
 
