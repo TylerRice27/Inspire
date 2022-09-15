139151d0c528f4eb429720de5827cfa79c4ffccf
TylerRice27 <Tylerrayrice2@gmail.com>
2022-09-15 10:17:12 -0600

    styling clock

--- app/App.js
+++ app/App.js
@@ -10,7 +10,7 @@ function currentTime() {
   let date = new Date();
   let hh = date.getHours();
   let mm = date.getMinutes();
-  let ss = date.getSeconds();
+  // let ss = date.getSeconds();
   let session = "AM";
 
   if (hh == 0) {
@@ -23,9 +23,9 @@ function currentTime() {
 
   hh = (hh < 10) ? "0" + hh : hh;
   mm = (mm < 10) ? "0" + mm : mm;
-  ss = (ss < 10) ? "0" + ss : ss;
+  // ss = (ss < 10) ? "0" + ss : ss;
 
-  let time = hh + ":" + mm + ":" + ss + " " + session;
+  let time = hh + ":" + mm + session;
 
   document.getElementById("clock").innerText = time;
   let t = setTimeout(function () { currentTime() }, 1000);
@@ -35,12 +35,11 @@ currentTime();
 
 class App {
   // valuesController = new ValuesController();
-
-
-
   todosController = new TodosController()
   quotesController = new QuotesController()
   weathersController = new WeathersController()
+
+
 
 
 
