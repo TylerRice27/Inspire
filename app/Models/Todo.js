38d5e81087b5ff1aab9280eeb37f2f18b0982b1e
TylerRice27 <Tylerrayrice2@gmail.com>
2022-09-15 18:03:48 -0600

    Working on getting todo numbers

--- app/Models/Todo.js
+++ app/Models/Todo.js
@@ -15,7 +15,7 @@ export class Todo {
     get Template() {
         return /*html*/ `
         <div class="mb-3 form-check">
-            <input type="checkbox" ${this.completed ? 'completed' : ''} onclick="app.todosController.editTodo('${this.id}')"  class="form-check-input" id="exampleCheck1">
+            <input type="checkbox" ${this.completed ? 'checked' : ''} onclick="app.todosController.editTodo('${this.id}')"  class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">${this.description}</label>
                           <i onclick="app.todosController.deleteTodo('${this.id}')" class="mdi mdi-delete text-danger fs-5"></i>
 
